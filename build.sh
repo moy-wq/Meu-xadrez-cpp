bc085adfc3b84e74d5ad9eef883cbe3f462c9364
Pedro Moy <pedrohenriquemoy21@gmail.com>
2026-06-05 15:43:07 -0300

    fix: conserto no arquivo de build

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-g++ tests/main_test.cpp src/UI/*.cpp src/app/*.cpp src/core/*.cpp -o xadrez.o -I include -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
-./xadrez
+g++ main_test.cpp src/UI/*.cpp src/app/*.cpp src/core/*.cpp -o xadrez.o -I include -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+./xadrez.o
